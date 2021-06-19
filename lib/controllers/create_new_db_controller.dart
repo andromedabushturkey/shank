b950437bad5889b894312df071defa33ebc6cade
chef <chef@d33tdev.ca>
2021-06-19 10:13:03 -0400

    cleaned up create_new_db_controller

--- lib/controllers/create_new_db_controller.dart
+++ lib/controllers/create_new_db_controller.dart
@@ -16,11 +16,6 @@ class CreateNewDbController extends GetxController {
 
   Database? _activeDB;
   var _listOfAvailDB = <FileSystemEntity>[].obs;
-  RxBool _passwordOneObscureSet = true.obs;
-  RxBool _passwordTwoObscureSet = true.obs;
-  RxnString _dbNameErrorMsg = RxnString();
-  RxnString _dbPasswordOneErrorMsg = RxnString();
-  RxnString _dbPasswordTwoErrorMsg = RxnString();
   var _tableName = ''.obs;
   //TextEditingControllers for creating databasedf
   final TextEditingController databaseNameController = TextEditingController();
@@ -28,59 +23,18 @@ class CreateNewDbController extends GetxController {
   final TextEditingController passwordTwoController = TextEditingController();
 
   Database? get activeDB => this._activeDB;
-  String? get dbNameError => this._dbNameErrorMsg.value;
-  String? get dbPasswordOneErrorMsg => this._dbPasswordOneErrorMsg.value;
-  String? get dbPasswordTwoErrorMsg => this._dbPasswordTwoErrorMsg.value;
-  bool get passwordOneObscureSet => this._passwordOneObscureSet.value;
-  bool get passwordTwoObscureSet => this._passwordTwoObscureSet.value;
   String get tableName => this._tableName.value;
 
   //getter that contains list of available databases
   List<FileSystemEntity> get listOfAvailDb => this._listOfAvailDB;
 
   set setActiveDB(value) => this._activeDB = value;
-  set setPasswordOneObscure(value) => this._passwordOneObscureSet.value = value;
-  set setPasswordTwoObscure(value) => this._passwordTwoObscureSet.value = value;
-
-  set setDbNameError(value) => this._dbNameErrorMsg.value = value;
-
-  set setDbPasswordOneErrorMsg(value) =>
-      this._dbPasswordOneErrorMsg.value = value;
-
-  set setDbpasswordTwoErrorMsg(value) =>
-      this._dbPasswordTwoErrorMsg.value = value;
   set setListOfAvailDB(value) => this._listOfAvailDB.add(value);
   set removeDbOfAvailDB(value) => this._listOfAvailDB.removeAt(value);
 
   set tableName(value) => this._tableName.value = value;
 
   //Validate the DB creation form to make sure the info provided is complete
-  bool validateNewDatabaseForm() {
-    if (databaseNameController.text.isEmpty) {
-      _dbNameErrorMsg.value = "Field can't be left empty";
-      return false;
-    }
-
-    if (passwordOneController.text.isEmpty) {
-      _dbPasswordOneErrorMsg.value = "Field can't be left empty";
-      return false;
-    }
-
-    if (passwordTwoController.text.isEmpty) {
-      _dbPasswordTwoErrorMsg.value = "Field can't be left empty";
-      return false;
-    }
-
-    if (passwordOneController.text != passwordTwoController.text) {
-      _dbPasswordOneErrorMsg.value = 'Passwords do not match';
-      _dbPasswordTwoErrorMsg.value = 'Passwords do not match';
-      return false;
-    } else {
-      _dbPasswordOneErrorMsg.value = '';
-      _dbPasswordTwoErrorMsg.value = '';
-    }
-    return true;
-  }
 
   void getExistingDB() async {
     Directory _dir = Directory("/data/user/0/com.example.shank/databases");
